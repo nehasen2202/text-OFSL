d9bb64b6ec2feded7dd3bf6e11a406097a711c27
nehasen2202 <n.sen.1591@outlook.com>
2026-06-07 20:23:25 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -18,41 +18,29 @@ model_dict = dict(
 )
 
 data_dir = dict(
-    CUB=os.getcwd() + '/filelists/CUB/',
-    miniImagenet=os.getcwd() + '/filelists/miniImagenet/',
-    omniglot=os.getcwd() + '/filelists/omniglot/',
-    emnist=os.getcwd() + '/filelists/emnist/',
-    cifar=os.getcwd() + '/filelists/cifar/',
-    fc100=os.getcwd() + '/filelists/fc100/',
-    tieredImagenet=os.getcwd() + '/filelists/tieredImagenet/',
+    ND0=os.getcwd() + '/filelists/ND0/',
+    ND1=os.getcwd() + '/filelists/ND1/',
+    ND2=os.getcwd() + '/filelists/ND2/',
+    ND3=os.getcwd() + '/filelists/ND3/',
 )
 
-start_epoch = 0  # Starting epoch
-train_n_way = 5  # class num to classify for training
-test_n_way = 5  # class num to classify for testing (validation)
+start_epoch = 0
+train_n_way = 5
+test_n_way = 5
 num_workers = 8
 test_iter_num = 600
 adaptation = False
-
-'''if torch.cuda.is_available():
-    use_cuda = True
-    print('GPU detected, running with GPU!')
-else:
-    print('GPU not detected, running with CPU!')
-    use_cuda = False'''
-    
-   
-#device = torch.device("cuda")
-#print('GPU detected, running with GPU!')
-
 
 
 def set_seed(seed=0):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
@@ -63,24 +51,17 @@ set_seed(seed)
 
 def get_stop_epoch(algorithm, dataset, n_shot=5):
     if algorithm in ['baseline', 'baseline++']:
-        if dataset in ['omniglot', 'cross_char']:
-            stop_epoch = 5
-        elif dataset in ['CUB']:
-            stop_epoch = 200  # This is different as stated in the open-review paper. However, using 400 epoch in baseline actually lead to over-fitting
-        else:
-            stop_epoch = 400  # default
+        stop_epoch = 400
     else:
         if n_shot == 5:
             stop_epoch = 400
         else:
-            stop_epoch = 600  # default
+            stop_epoch = 600
     return stop_epoch
 
 
 def get_image_size(model_name, dataset):
-    if 'Conv' in model_name and dataset in ['omniglot', 'cross_char']:
-        image_size = 28
-    elif 'Conv' in model_name or model_name == 'ResNet12':
+    if 'Conv' in model_name or model_name == 'ResNet12':
         image_size = 84
     else:
         image_size = 224
@@ -88,77 +69,98 @@ def get_image_size(model_name, dataset):
 
 
 def get_train_files(dataset):
-    if dataset == 'cross':
-        base_file = data_dir['miniImagenet'] + 'all.json'
-        val_file = data_dir['CUB'] + 'val.json'
-    elif dataset == 'cross_char':
-        base_file = data_dir['omniglot'] + 'noLatin.json'
-        val_file = data_dir['emnist'] + 'val.json'
-    else:
-        base_file = data_dir[dataset] + 'base.json'
-        val_file = data_dir[dataset] + 'val.json'
+    base_file = data_dir[dataset] + 'base.json'
+    val_file = data_dir[dataset] + 'val.json'
     return base_file, val_file
 
 
-def get_train_loader(algorithm, image_size, base_file, val_file, train_n_way=5, test_n_way=5, n_shot=5,
-                     num_workers=num_workers):
+def get_train_loader(
+    algorithm,
+    image_size,
+    base_file,
+    val_file,
+    train_n_way=5,
+    test_n_way=5,
+    n_shot=5,
+    num_workers=num_workers
+):
     if 'baseline' in algorithm:
-        base_datamgr = SimpleDataManager(image_size, batch_size=16, num_workers=num_workers)
+        base_datamgr = SimpleDataManager(
+            image_size,
+            batch_size=16,
+            num_workers=num_workers
+        )
         base_loader = base_datamgr.get_data_loader(base_file, aug=True)
-        val_datamgr = SimpleDataManager(image_size, batch_size=64, num_workers=num_workers)
+
+        val_datamgr = SimpleDataManager(
+            image_size,
+            batch_size=64,
+            num_workers=num_workers
+        )
         val_loader = val_datamgr.get_data_loader(val_file, aug=False)
+
     else:
-        n_query = max(1, int(
-            16 * test_n_way / train_n_way))  # if test_n_way <train_n_way, reduce n_query to keep batch size small
-        base_datamgr = SetDataManager(image_size, n_query=n_query, n_way=train_n_way, n_support=n_shot,
-                                      num_workers=num_workers)  # n_eposide=100
+        n_query = max(1, int(16 * test_n_way / train_n_way))
+
+        base_datamgr = SetDataManager(
+            image_size,
+            n_query=n_query,
+            n_way=train_n_way,
+            n_support=n_shot,
+            num_workers=num_workers
+        )
         base_loader = base_datamgr.get_data_loader(base_file, aug=True)
-        val_datamgr = SetDataManager(image_size, n_query=n_query, n_way=test_n_way, n_support=n_shot,
-                                     num_workers=num_workers)
+
+        val_datamgr = SetDataManager(
+            image_size,
+            n_query=n_query,
+            n_way=test_n_way,
+            n_support=n_shot,
+            num_workers=num_workers
+        )
         val_loader = val_datamgr.get_data_loader(val_file, aug=False)
-        # a batch for SetDataManager: a [n_way, n_support + n_query, dim, w, h] tensor
+
     return base_loader, val_loader
 
 
 def get_novel_file(dataset, split='novel'):
-    if dataset == 'cross':
-        if split == 'base':
-            loadfile = data_dir['miniImagenet'] + 'all.json'
-        else:
-            loadfile = data_dir['CUB'] + split + '.json'
-    elif dataset == 'cross_char':
-        if split == 'base':
-            loadfile = data_dir['omniglot'] + 'noLatin.json'
-        else:
-            loadfile = data_dir['emnist'] + split + '.json'
-    else:
-        loadfile = data_dir[dataset] + split + '.json'
+    loadfile = data_dir[dataset] + split + '.json'
     return loadfile
 
 
 def get_model_name(model_name, dataset):
-    if dataset in ['omniglot', 'cross_char']:
-        assert model_name == 'Conv4', 'omniglot only support Conv4 without augmentation'
-        model_name = 'Conv4S'
     return model_name
 
 
 def get_resume_file(checkpoint_dir, epoch=None):
     filelist = glob.glob(os.path.join(checkpoint_dir, '*.tar'))
+
     if len(filelist) == 0:
         return None
+
     if epoch is not None:
         resume_file = os.path.join(checkpoint_dir, '{:d}.tar'.format(epoch))
         return resume_file
-    filelist = [x for x in filelist if os.path.basename(x) != 'best_model.tar']
-    epochs = np.array([int(os.path.splitext(os.path.basename(x))[0]) for x in filelist])
+
+    filelist = [
+        x for x in filelist
+        if os.path.basename(x) != 'best_model.tar'
+    ]
+
+    epochs = np.array([
+        int(os.path.splitext(os.path.basename(x))[0])
+        for x in filelist
+    ])
+
     max_epoch = np.max(epochs)
     resume_file = os.path.join(checkpoint_dir, '{:d}.tar'.format(max_epoch))
+
     return resume_file
 
 
 def get_best_file(checkpoint_dir):
     best_file = os.path.join(checkpoint_dir, 'best_model.tar')
+
     if os.path.isfile(best_file):
         return best_file
     else:
@@ -166,45 +168,69 @@ def get_best_file(checkpoint_dir):
 
 
 def get_num_class(dataset):
-    if dataset == 'omniglot':
-        num_classes = 4112  # total number of classes in softmax, only used in baseline
-    elif dataset == 'cross_char':
-        num_classes = 1597
-    elif dataset in ['cifar', 'fc100', 'miniImagenet', 'cross']:
-        num_classes = 100
-    elif dataset == 'tieredImagenet':
-        num_classes = 608
+    if dataset in ['ND0', 'ND1', 'ND2', 'ND3']:
+        num_classes = 7
     else:
-        num_classes = 200
+        raise ValueError(f"Unknown dataset: {dataset}")
+
     return num_classes
 
 
 def get_model(algorithm, model_name, dataset, n_way, n_shot, adaptation):
     num_classes = get_num_class(dataset)
+
     if algorithm == 'baseline':
-        model = Baseline(model_dict[model_name], n_way=n_way, n_support=n_shot, num_class=num_classes,
-                         loss_type='softmax', )
+        model = Baseline(
+            model_dict[model_name],
+            n_way=n_way,
+            n_support=n_shot,
+            num_class=num_classes,
+            loss_type='softmax',
+        )
+
     elif algorithm == 'baseline++':
-        model = Baseline(model_dict[model_name], n_way=n_way, n_support=n_shot, num_class=num_classes,
-                         loss_type='dist', )
+        model = Baseline(
+            model_dict[model_name],
+            n_way=n_way,
+            n_support=n_shot,
+            num_class=num_classes,
+            loss_type='dist',
+        )
+
     else:
         raise ValueError('Unknown algorithm')
+
     return model
 
 
-def get_checkpoint_dir(algorithm, model_name, dataset, train_n_way, n_shot, addition=None):
-    if dataset == 'cross':
-        dataset = 'miniImagenet'
-    elif dataset == 'cross_char':
-        dataset = 'omniglot'
+def get_checkpoint_dir(
+    algorithm,
+    model_name,
+    dataset,
+    train_n_way,
+    n_shot,
+    addition=None
+):
     if addition is None:
-        checkpoint_dir = base_path + '/save/checkpoints/%s/%s_%s' % (dataset, model_name, algorithm)
+        checkpoint_dir = base_path + '/save/checkpoints/%s/%s_%s' % (
+            dataset,
+            model_name,
+            algorithm
+        )
     else:
-        checkpoint_dir = base_path + '/save/checkpoints/%s/%s_%s_%s' % (dataset, model_name, algorithm, str(addition))
-    if not algorithm in ['baseline', 'baseline++']:
+        checkpoint_dir = base_path + '/save/checkpoints/%s/%s_%s_%s' % (
+            dataset,
+            model_name,
+            algorithm,
+            str(addition)
+        )
+
+    if algorithm not in ['baseline', 'baseline++']:
         checkpoint_dir += '_%dway_%dshot' % (train_n_way, n_shot)
+
     if not os.path.isdir(checkpoint_dir):
         os.makedirs(checkpoint_dir, exist_ok=True)
+
     return checkpoint_dir
 
 
@@ -214,4 +240,8 @@ def get_optimizer(model, model_name):
 
 
 def one_hot(y, num_class):
-    return torch.zeros((len(y), num_class)).scatter_(1, y.unsqueeze(1).long(), 1)
+    return torch.zeros((len(y), num_class)).scatter_(
+        1,
+        y.unsqueeze(1).long(),
+        1
+    )
